cfa811254a7a753d1f03d0f5e49d49bdb7d328f5
yuvraj1409 <69112113+yuvraj1409@users.noreply.github.com>
2020-12-31 17:58:25 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+# calculator 
+
+operator = input("enter your operator '+','-','*'z'/':"
+num1 = int(input("enter your first number:")
+num2 = int(input("enter your secand number:")
+
+if operator = "+":
+   print( num1 +num2)
+elif operator = "-":
+   print( num1- num2)
+elif operator = "*":
+   print( num1 * num2)
+elif operator = "/":
+   print( num1 / num2)
+else :
+   print("Are you kidding me!!!")
+
